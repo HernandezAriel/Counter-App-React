96d5aa744fc3961d154075242a5dabcaffcd0961
Cynad97 <cynad97@gmail.com>
2021-12-28 16:41:32 -0300

    Counter-App React

---
+++ src/FirstApp.js
@@ -0,0 +1,15 @@
+//FC
+import React from 'react';
+
+const FirstApp = () => {
+
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>First App</p>
+        </>
+    );
+
+}
+
+export default FirstApp;
